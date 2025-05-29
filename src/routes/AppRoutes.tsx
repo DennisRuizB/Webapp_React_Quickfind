45cfd8bb72ecf9fa92d1986f38589a6c2ebc4d1e
MohaLourd <mohamed.lourdjane@estudiantat.upc.edu>
2025-05-29 10:22:56 +0200

    Minim2, un usuari por fer, sugerencia, valoracio o reportar error

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "../components/Login/Login";
-import Home from "../components/MainPages/Home/Home";
-import Perfil from "../components/Profiles/Perfil/Perfil";
-import Services from "../components/MainPages/NavBar_Services/NavBar_Services";
-import CompanyPerfil from "../components/Profiles/CompamyPerfil/CompanyPerfil";
-import PerfilExterno from "../components/Profiles/PerfilExterno/PerfilExterno";
-import ReserveProducts from "../components/ReserveProducts/ReserveProducts";
-import CartPage from "../components/MainPages/CartPage/CartPage";
-import CompanyPerfilManage from "../components/CompamyPerfilManage/CompanyPerfilManage";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from '../components/Login/Login';
+import Home from '../components/MainPages/Home/Home';
+import Perfil from '../components/Profiles/Perfil/Perfil';
+import Services from '../components/MainPages/NavBar_Services/NavBar_Services';
+import CompanyPerfil from '../components/Profiles/CompamyPerfil/CompanyPerfil';
+import PerfilExterno from '../components/Profiles/PerfilExterno/PerfilExterno';
+import ReserveProducts from '../components/ReserveProducts/ReserveProducts';
+import CartPage from '../components/MainPages/CartPage/CartPage';
+import CompanyPerfilManage from '../components/CompamyPerfilManage/CompanyPerfilManage';
+import Contact from '../components/MainPages/Contact/Contact';
 
 const AppRoutes = () => {
   return (
@@ -23,6 +24,7 @@ const AppRoutes = () => {
       <Route path="/ReserveProducts/:id" element={<ReserveProducts />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/companyManage/:id" element={<CompanyPerfilManage />} />
+      <Route path="/contact" element={<Contact />} />
       {/* Redirige a la página de inicio si la ruta no coincide */}
     </Routes>
   );
